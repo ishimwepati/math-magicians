a7f67d90ea674e5935e921b8f08c751e45eb72b4
ishimwepati <wazacode@gmail.com>
2023-08-16 21:48:04 +0300

    Design fixed

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,9 +7,26 @@ function Calculator() {
     <div className="calculator">
       <div className="display">0</div>
       <div className="buttons">
+        <Button label="AC" />
+        <Button label="+/-" />
+        <Button label="%" />
+        <Button label="÷" />
         <Button label="7" />
         <Button label="8" />
-        {/* Add more buttons here */}
+        <Button label="9" />
+        <Button label="x" />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" />
+        <Button label="" />
+        <Button label="0" />
+        <Button label="." />
+        <Button label="=" />
       </div>
     </div>
   );
