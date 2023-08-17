48dbaeefcda4d906fe43cdddd18f15ead4dd0f77
ishimwepati <wazacode@gmail.com>
2023-08-17 15:45:42 +0300

    Functionality done

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.css';
 
-function Button({ label, isOperator, gridColumnSpan }) {
+function Button({
+  label, isOperator, gridColumnSpan, onClick,
+}) {
   const buttonClassName = isOperator ? 'button operator' : 'button';
   const buttonStyle = gridColumnSpan ? { gridColumn: `span ${gridColumnSpan}` } : {};
 
   return (
-    <button className={buttonClassName} style={buttonStyle} type="button">
+    <button className={buttonClassName} style={buttonStyle} type="button" onClick={() => onClick(label)}>
       {label}
     </button>
   );
@@ -17,6 +19,7 @@ Button.propTypes = {
   label: PropTypes.string.isRequired,
   isOperator: PropTypes.bool,
   gridColumnSpan: PropTypes.number,
+  onClick: PropTypes.func.isRequired, // Add this line to specify onClick as a required prop
 };
 
 Button.defaultProps = {
