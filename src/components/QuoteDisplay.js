d1e44361aeee5b5323b36d46fe017400a9663eb0
ishimwepati <wazacode@gmail.com>
2023-08-17 23:09:57 +0300

    Quotes design

--- src/components/QuoteDisplay.js
+++ src/components/QuoteDisplay.js
@@ -1,62 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './QuoteDisplay.css';
 
 function QuoteDisplay() {
-  const [quote, setQuote] = useState(null);
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    async function fetchQuote() {
-      try {
-        setLoading(true);
-        const response = await axios.get('https://api.api-ninjas.com/v1/quotes?category=happiness', {
-          headers: {
-            'X-Api-Key': 'Ynra9XHiA4ZSA+O1ZPEx8Q==r5YLGKlKiQnALROo',
-          },
-        });
+    const apiUrl = 'https://api.api-ninjas.com/v1/quotes?category=happiness';
 
-        if (response.data.length === 0) {
-          throw new Error('No quotes found');
-        }
+    const headers = {
+      'X-Api-Key': 'Ynra9XHiA4ZSA+O1ZPEx8Q==r5YLGKlKiQnALROo',
+    };
 
-        setQuote(response.data[0]);
+    axios.get(apiUrl, { headers })
+      .then((response) => {
+        const { quote, author } = response.data[0];
+        setQuote(quote);
+        setAuthor(author);
         setLoading(false);
-      } catch (err) {
-        setError(err);
+      })
+      .catch((error) => {
+        console.error('Error fetching quote:', error);
         setLoading(false);
-      }
-    }
-
-    fetchQuote();
+      });
   }, []);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return (
-      <p>
-        Error:
-        {error.message}
-      </p>
-    );
-  }
-
-  if (quote) {
-    return (
-      <div className="quote">
-        <p>{quote.quote}</p>
-        <p>
-          -
-          {quote.author}
-        </p>
-      </div>
-    );
-  }
-
-  return null;
+  return (
+    <div className="quote-container">
+      {loading && <p>Loading...</p>}
+      {!loading && (
+        <div className="quote">
+          <p className="quote-text">{quote}</p>
+          <p className="quote-author">
+            -
+            {author}
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default QuoteDisplay;
