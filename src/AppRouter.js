a40a93b26aa69b84be8d14c930674b7a6da0184e
ishimwepati <wazacode@gmail.com>
2023-08-23 18:17:21 +0300

    Working on the design

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -9,29 +9,34 @@ import './AppRouter.css'; // Import the CSS file
 
 const AppRouter = () => (
   <Router>
-    <div className="navbar">
-      {/* Navigation Links */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home |</Link>
-          </li>
-          <li>
-            <Link to="/calculator">Calculator |</Link>
-          </li>
-          <li>
-            <Link to="/quote">Quote</Link>
-          </li>
-        </ul>
-      </nav>
-
-      {/* Define Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<Calculator />} />
-        <Route path="/quote" element={<Quote />} />
-      </Routes>
+    <div className="header">
+      <div>
+        <h1>Math Magicians</h1>
+      </div>
+      <div className="navbar">
+        {/* Navigation Links */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home |</Link>
+            </li>
+            <li>
+              <Link to="/calculator">Calculator |</Link>
+            </li>
+            <li>
+              <Link to="/quote">Quote</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
+
+    {/* Define Routes */}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
   </Router>
 );
 
